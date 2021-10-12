cb4297471ae43926f3f6a1ed225efeabcdbeb931
kp95 <penduanttal@gmail.com>
2021-10-12 11:02:11 -0400

    first commit

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/CustomerUserDetails.java
@@ -0,0 +1,68 @@
+package com.shopme.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.shopme.common.entity.Customer;
+
+public class CustomerUserDetails implements UserDetails{
+
+	private Customer customer;
+	
+	public CustomerUserDetails(Customer customer) {
+		// TODO Auto-generated constructor stub
+		this.customer = customer;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.customer.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.customer.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return this.customer.isEnabled();
+	}
+
+	public String getFullName() {
+		return customer.getFirstName() +" " + customer.getLastName();
+	}
+	
+	public Customer getCustomer() {
+		return this.customer;
+	}
+}
